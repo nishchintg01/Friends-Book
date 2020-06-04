07e6452cb34e2f793d5cb7d055d46812a5f3d7ee
nishchintg01 <nishchintg01@gmail.com>
2020-06-04 19:25:05 +0530

    changed wsgi.py

--- FriendsBook/wsgi.py
+++ FriendsBook/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Freakapp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FriendsBook.settings')
 
 application = get_wsgi_application()
