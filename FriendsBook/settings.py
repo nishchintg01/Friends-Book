d31b901ed4ae3f61326c0c7e8655e573300ff453
nishchint gauniyal <nishchintg01@gmail.com>
2020-09-20 02:33:33 +0530

    Update settings.py

--- FriendsBook/settings.py
+++ FriendsBook/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["friendsbooknish.herokuapp.com","localhost","nish01.pythonanywhere.com"]
+ALLOWED_HOSTS = ["localhost"]
 # Application definition
 
 INSTALLED_APPS = [
